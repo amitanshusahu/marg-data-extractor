75b262162eb754708134283fbf879c37f4010302
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2025-04-05 07:07:30 +0530

    salt

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
+# install dependency
 pip install pymupdf pillow watchdog pywin32 requests
+
+
+# build
+pyinstaller --clean --onefile --windowed --icon=app.ico --name=NexInsights --add-data "logo.png;." main.py
